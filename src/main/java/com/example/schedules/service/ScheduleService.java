5eb8a5718dd977b90d131bc6783aa723a8cb55da
Ha-minsang <gkalstkd@gmail.com>
2025-11-03 14:21:22 +0900

    Service 기본 틀 생성

---
+++ src/main/java/com/example/schedules/service/ScheduleService.java
@@ -0,0 +1,12 @@
+package com.example.schedules.service;
+
+import com.example.schedules.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+
+    private final ScheduleRepository scheduleRepository;
+}
