1d9921f69cc19a6c49cbb5c2b6179dfe288a0f0a
Ha-minsang <gkalstkd@gmail.com>
2025-11-03 14:20:56 +0900

    Controller 기본 틀 생성

---
+++ src/main/java/com/example/schedules/controller/ScheduleController.java
@@ -0,0 +1,12 @@
+package com.example.schedules.controller;
+
+import com.example.schedules.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+}
